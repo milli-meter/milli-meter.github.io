10d70b83531fd7ae18e054818bb6989cd51ed120
Ngoc Duc <71770712+milli-meter@users.noreply.github.com>
2020-09-23 20:33:37 +0700

    upload

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ AOS.init({
   startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
   initClassName: 'aos-init', // class applied after initialization
   animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  useClassNames: true, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: true, // disables automatic mutations' detections (advanced)
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
   
@@ -17,8 +17,6 @@ AOS.init({
   easing: 'ease', // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
   mirror: true, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
 });
 
 
