c246d2ffd41a69680c57db68a09fd754d1d5db8c
Ngoc Duc <71770712+milli-meter@users.noreply.github.com>
2020-09-23 20:17:41 +0700

    upload

---
+++ main.js
@@ -0,0 +1,52 @@
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 800, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: true, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+/* Active and remove menu */
+
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+    /* Active link */
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
+  /* Hide when choose */
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show');
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
